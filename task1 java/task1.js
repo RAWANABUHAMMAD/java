94a1b9990b1a5734f47afb15945122ba9c4ffc5c
RAWANABUHAMMAD <rawan.abuhammad89@gmail.com>
2024-12-29 21:16:16 +0300

    intial

---
+++ task1 java/task1.js
@@ -0,0 +1,45 @@
+//1//
+let cash = 1000;
+let currentLiabilities = 500;
+document.write ( cash / currentLiabilities+ "<br>");
+
+//2//
+let revenues = 1000;
+let expenses = 500;
+document.write( revenues - expenses+ "<br>");
+
+//3//
+let liabilities = 1000;
+let equity = 500;
+document.write(liabilities + equity+ "<br>");
+
+//4//
+let profitMargin = 1000;  
+let sales = 500; 
+document.write(profitMargin * sales+ "<br>");
+
+//5//
+let x=7;
+let y=9;
+let z=2;
+document.write((x+y+z)/3 +"<br>" );
+
+
+//6//
+let price = 150;
+let discountPercentage = 30;
+document.write( price - (price * (discountPercentage / 100))+ "<br>");
+
+//7//
+let age = 20;
+document.write((age > 18 && age < 30)+ "<br>");
+
+//8//
+let number1 = 2;
+let number2 = 3;
+document.write (Math.pow(2, 3)+ "<br>");
+
+//9//
+let num1 = 10;
+let num2 = 4;
+document.write(num1 % num2+ "<br>");
